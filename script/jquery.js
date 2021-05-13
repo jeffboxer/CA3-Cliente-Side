5d1b8a31078dff6ed23ea0234f02fd0038d10f5d
jeffboxer <74919852+jeffboxer@users.noreply.github.com>
2021-05-13 22:32:41 +0100

    Add files via upload
    Created a new script for my Page

---
+++ script/jquery.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#img1").mouseover(function(){
+      $("#visible1").removeClass("hidden");
+    });
+    $("#img1").mouseout(function(){
+      $("#visible1").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img2").mouseover(function(){
+      $("#visible2").removeClass("hidden");
+    });
+    $("#img2").mouseout(function(){
+      $("#visible2").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img3").mouseover(function(){
+      $("#visible3").removeClass("hidden");
+    });
+    $("#img3").mouseout(function(){
+      $("#visible3").addClass("hidden");
+    });
+  });
