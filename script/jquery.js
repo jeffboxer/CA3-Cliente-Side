3fb59ae2cd783e9b129999ad5673a4c10e8bd7d0
jeffboxer <74919852+jeffboxer@users.noreply.github.com>
2021-05-15 17:46:10 +0100

    Add files via upload
    Previous files were not being updated

---
+++ script/jquery.js
@@ -0,0 +1,107 @@
+$(document).ready(function(){
+    $("#img1").mouseover(function(){
+      $("#visible1").removeClass("hidden");
+    });
+    $("#img1").mouseout(function(){
+      $("#visible1").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img2").mouseover(function(){
+      $("#visible2").removeClass("hidden");
+    });
+    $("#img2").mouseout(function(){
+      $("#visible2").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img3").mouseover(function(){
+      $("#visible3").removeClass("hidden");
+    });
+    $("#img3").mouseout(function(){
+      $("#visible3").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img4").mouseover(function(){
+      $("#visible4").removeClass("hidden");
+    });
+    $("#img4").mouseout(function(){
+      $("#visible4").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img5").mouseover(function(){
+      $("#visible5").removeClass("hidden");
+    });
+    $("#img5").mouseout(function(){
+      $("#visible5").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img6").mouseover(function(){
+      $("#visible6").removeClass("hidden");
+    });
+    $("#img6").mouseout(function(){
+      $("#visible6").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img7").mouseover(function(){
+      $("#visible7").removeClass("hidden");
+    });
+    $("#img7").mouseout(function(){
+      $("#visible7").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img8").mouseover(function(){
+      $("#visible8").removeClass("hidden");
+    });
+    $("#img8").mouseout(function(){
+      $("#visible8").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img9").mouseover(function(){
+      $("#visible9").removeClass("hidden");
+    });
+    $("#img9").mouseout(function(){
+      $("#visible9").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img10").mouseover(function(){
+      $("#visible10").removeClass("hidden");
+    });
+    $("#img10").mouseout(function(){
+      $("#visible10").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img11").mouseover(function(){
+      $("#visible11").removeClass("hidden");
+    });
+    $("#img11").mouseout(function(){
+      $("#visible11").addClass("hidden");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#img12").mouseover(function(){
+      $("#visible12").removeClass("hidden");
+    });
+    $("#img12").mouseout(function(){
+      $("#visible12").addClass("hidden");
+    });
+  });
